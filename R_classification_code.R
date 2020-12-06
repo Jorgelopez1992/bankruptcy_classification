fced185e73e1449e764eb54dc2213b979898cace
jorgelopezgama1992 <jorgelopezgama1992@users.noreply.github.com>
2020-12-06 06:42:11 -0500

    Update R_classification_code.R

--- R_classification_code.R
+++ R_classification_code.R
@@ -11,6 +11,7 @@ library(ROCR)
 library(caret)
 library(pROC)
 library(randomForest)  
+library(gridExtra)
 
 #downloading data directly from github repository 
 data <- read_csv('https://raw.githubusercontent.com/Jorgelopez1992/bankruptcy_classification/master/bankruptcy_Train.csv',
@@ -27,7 +28,7 @@ p        =    ncol(X)
 n.train        =     floor(0.9*n)
 n.test         =     n-n.train
 
-M              =     2   #total loops 
+M              =     50   #total loops 
 num_thrs       =     100  #Number of threshholds for classification for logistic regression (0.01,0.02 etc.)
 
 
@@ -58,6 +59,17 @@ rid.train.error <- c(rep(0,M))
 rid.test.error <-c(rep(0,M))
 rf.train.error <- c(rep(0,M))
 rf.test.error <-c(rep(0,M))
+
+#Vectors to store AUC
+elas.train.auc <- c(rep(0,M))
+elas.test.auc <-c(rep(0,M))
+las.train.auc <- c(rep(0,M))
+las.test.auc <-c(rep(0,M))
+rid.train.auc <- c(rep(0,M))
+rid.test.auc <-c(rep(0,M))
+rf.train.auc <- c(rep(0,M))
+rf.test.auc <-c(rep(0,M))
+
 
 for (m in c(1:M)) {
   #randomly sampling rows 
@@ -601,50 +613,155 @@ for (m in c(1:M)) {
 
 
 #creating dataframes to with auc data
-elas.train.df<-data.frame(elas.train.auc,c(rep('elastic_net',M)),c(rep('train',M)))
+elas.train.df<-data.frame(elas.train.auc,c(rep('Elastic Net',M)),c(rep('Train',M)))
 colnames(elas.train.df)<-c('auc','model','set')
-elas.test.df<-data.frame(elas.test.auc,c(rep('elastic_net',M)),c(rep('test',M)))
+elas.test.df<-data.frame(elas.test.auc,c(rep('Elastic Net',M)),c(rep('Test',M)))
 colnames(elas.test.df)<-c('auc','model','set')
 
-rid.train.df<-data.frame(rid.train.auc,c(rep('ridge',M)),c(rep('train',M)))
+rid.train.df<-data.frame(rid.train.auc,c(rep('Ridge',M)),c(rep('Train',M)))
 colnames(rid.train.df)<-c('auc','model','set')
-rid.test.df<-data.frame(rid.test.auc,c(rep('ridge',M)),c(rep('test',M)))
+rid.test.df<-data.frame(rid.test.auc,c(rep('Ridge',M)),c(rep('Test',M)))
 colnames(rid.test.df)<-c('auc','model','set')
 
-las.train.df<-data.frame(las.train.auc,c(rep('lasso',M)),c(rep('train',M)))
+las.train.df<-data.frame(las.train.auc,c(rep('Lasso',M)),c(rep('Train',M)))
 colnames(las.train.df)<-c('auc','model','set')
-las.test.df<-data.frame(las.test.auc,c(rep('lasso',M)),c(rep('test',M)))
+las.test.df<-data.frame(las.test.auc,c(rep('Lasso',M)),c(rep('Test',M)))
 colnames(las.test.df)<-c('auc','model','set')
 
-rf.train.df<-data.frame(rf.train.auc,c(rep('random forest',M)),c(rep('train',M)))
+rf.train.df<-data.frame(rf.train.auc,c(rep('Random Forest',M)),c(rep('Train',M)))
 colnames(rf.train.df)<-c('auc','model','set')
-rf.test.df<-data.frame(rf.test.auc,c(rep('random forest',M)),c(rep('test',M)))
+rf.test.df<-data.frame(rf.test.auc,c(rep('Random Forest',M)),c(rep('Test',M)))
 colnames(rf.test.df)<-c('auc','model','set')
 
 
-#merging dataframes 
+#merging dataframes
 auc.df<-rbind(elas.train.df,elas.test.df,
-               rid.train.df,rid.test.df,
-               las.train.df,las.test.df,
-               rf.train.df,rf.test.df)
+              rid.train.df,rid.test.df,
+              las.train.df,las.test.df,
+              rf.train.df,rf.test.df)
 auc.df$auc<-as.numeric(auc.df$auc)
+
+auc.df$set<-factor(auc.df$set, levels=c('Train','Test'))
+auc.df$model<- factor(auc.df$model, levels=c('Elastic Net','Lasso','Ridge','Random Forest'))
 
 
 ggplot(auc.df,aes(x=model,y=auc,fill=model))+geom_boxplot()+
   facet_wrap(~set)+theme_light()+
   ggtitle('')+ylab('AUC')+xlab('Model')+
   theme(plot.title = element_text(hjust = 0.5))+
-  theme(axis.title.x=element_blank(),axis.text.x=element_blank())+
-  ggtitle('AUC Comparison')
+  theme(axis.title.x=element_blank())+
+  theme(axis.text.x=element_blank())
 
-elas_plot<-plot(elas.cv.fit)
-rid_plot<-plot(rid.cv.fit)
-las_plot<-plot(las.cv.fit)
 
+#### CV PLOTS
+par(mfrow=c(1,3))
+plot(elas.cv.fit, main = "Elastic Net")
+plot(las.cv.fit,main= 'Lasso') 
+plot(rid.cv.fit, main = "Ridge")
+par(mfrow=c(1,1))
+
+### TIMES REPORTS####
 cat('\n','elastic net cv time',toString(elas.cv.time[50]),'\n',
     'elastic net time',toString(elas.time[50]),'\n',
     'ridge cv time',toString(rid.cv.time[50]),'\n',
     'ridge net  time',toString(rid.time[50]),'\n',
     'lasso net cv time',toString(las.cv.time[50]),'\n',
-    'lasso net  time',toString(las.time[50]),'\n',
+    'lasso net  time',toString(las.time[50]),'\n', 
     'random forest time',toString(rf.time[50]),'\n')
+
+#90% interval AUC
+quantile( elas.test.auc,probs=c(0.05,0.95))
+quantile( rid.test.auc,probs=c(0.05,0.95))
+quantile( las.test.auc,probs=c(0.05,0.95))
+quantile( rf.test.auc,probs=c(0.05,0.95))
+
+# BAR PLOTS FOR COEFFICIENTS
+elas.betas <- as.vector(elas.fit$beta)
+las.betas <- as.vector(las.fit$beta)
+rid.betas <- as.vector(rid.fit$beta)
+rf.betas <- as.vector(rf.model$importance[,2])
+
+features<-c('net profit / total assets',
+  'total liabilities / total assets',
+  'working capital / total assets','current assets / short-term liabilities',
+  '[(cash + short-term securities + receivables - short-term liabilities) / (operating expenses - depreciation)] * 365',
+  'retained earnings / total assets','EBIT / total assets','book value of equity / total liabilities',
+  'sales / total assets','equity / total assets','(gross profit + extraordinary items + financial expenses) / total assets',
+  'gross profit / short-term liabilities','(gross profit + depreciation) / sales',
+  '(gross profit + interest) / total assets','(total liabilities * 365) / (gross profit + depreciation)',
+  '(gross profit + depreciation) / total liabilities','total assets / total liabilities','gross profit / total assets',
+  'gross profit / sales','(inventory * 365) / sales','sales (n) / sales (n-1)',
+  'profit on operating activities / total assets','net profit / sales','gross profit (in 3 years) / total assets',
+  '(equity - share capital) / total assets','(net profit + depreciation) / total liabilities',
+  'profit on operating activities / financial expenses','working capital / fixed assets','logarithm of total assets',
+  '(total liabilities - cash) / sales','(gross profit + interest) / sales',
+  '(current liabilities * 365) / cost of products sold','operating expenses / short-term liabilities',
+  'operating expenses / total liabilities','profit on sales / total assets','total sales / total assets',
+  '(current assets - inventories) / long-term liabilities','constant capital / total assets',
+  'profit on sales / sales','(current assets - inventory - receivables) / short-term liabilities',
+  'total liabilities / ((profit on operating activities + depreciation) * (12/365))',
+  'profit on operating activities / sales','rotation receivables + inventory turnover in days',
+  '(receivables * 365) / sales','net profit / inventory','(current assets - inventory) / short-term liabilities',
+  '(inventory * 365) / cost of products sold',
+  'EBITDA (profit on operating activities - depreciation) / total assets',
+  'EBITDA (profit on operating activities - depreciation) / sales',
+  'current assets / total liabilities','short-term liabilities / total assets',
+  '(short-term liabilities * 365) / cost of products sold)','equity / fixed assets',
+  'constant capital / fixed assets','working capital','(sales - cost of products sold) / sales',
+  '(current assets - inventory - short-term liabilities) / (sales - gross profit - depreciation)',
+  'total costs /total sales','long-term liabilities / equity','sales / inventory','sales / receivables',
+  '(short-term liabilities *365) / sales','sales / short-term liabilities','sales / fixed assets')
+
+betas.df<-data.frame(elas.betas,las.betas,rid.betas,rf.betas,features,stringsAsFactors=FALSE)
+
+ordered.df<-betas.df%>%arrange(desc(abs(betas.df$elas.betas)))
+
+elas.betas <- ordered.df$elas.betas
+las.betas <- ordered.df$las.betas
+rid.betas <- ordered.df$rid.betas
+rf.betas <- ordered.df$rf.betas
+features <- ordered.df$features
+colors <- rep(ifelse(elas.betas>1,1,0),4)
+
+Betas <- c(elas.betas,las.betas,rid.betas,rf.betas)
+Features <- c(rep(features,4))
+
+Models <- c(rep('Elastic Net',64),rep('Lasso',64),rep('Ridge',64),rep('Random Forest',64))
+coeffs <- data.frame(Betas,Models,Features,colors)
+
+colnames(coeffs)<-c('Coefficient','Model','Feature','Colors')
+
+ggplot(coeffs , aes(x = reorder(Feature,-abs(rep(elas.betas,4))), y=Coefficient,fill=as.factor(Colors))) +
+  geom_bar(stat = "identity", colour="black")+facet_grid(Model~.)+theme_minimal()+
+  theme(axis.title.x=element_blank(),legend.position = "none",axis.text.x =element_blank())
+
+
+#### COEFFICIENT PLOTS #######
+p1 <-ggplot(coeffs%>%filter(Model=='Elastic Net'),
+       aes(x = reorder(Feature,-abs(elas.betas)), y=Coefficient,fill=as.factor(Colors)))+
+         geom_bar(stat = "identity", fill='white', colour="black")+theme_minimal()+ylab('Elastic Net')+
+  theme(axis.title.x=element_blank(),legend.position = "none",axis.text.x =element_blank())+
+  ggtitle('Coefficient Values')
+
+p2 <-ggplot(coeffs%>%filter(Model=='Lasso'),
+       aes(x = reorder(Feature,-abs(elas.betas)), y=Coefficient,fill=as.factor(Colors)))+
+  geom_bar(stat = "identity", fill='white',colour="black")+theme_minimal()+ylab('Lasso')+
+  theme(axis.title.x=element_blank(),legend.position = "none",axis.text.x =element_blank())
+
+p3 <-ggplot(coeffs%>%filter(Model=='Ridge'),
+       aes(x = reorder(Feature,-abs(elas.betas)), y=Coefficient,fill=as.factor(Colors)))+
+  geom_bar(stat = "identity", fill='white', colour="black")+theme_minimal()+ylab('Ridge')+
+  theme(axis.title.x=element_blank(),legend.position = "none",axis.text.x =element_blank())
+
+p4 <-ggplot(coeffs%>%filter(Model=='Random Forest'),
+       aes(x = reorder(Feature,-abs(elas.betas)), y=Coefficient,fill=as.factor(Colors)))+
+  geom_bar(stat = "identity", fill='white', colour="black")+theme_minimal()+ylab('Random Forest')+
+  theme(axis.title.x=element_blank(),legend.position = "none",axis.text.x =element_blank())
+
+grid.arrange(p1, p2,p3,p4, ncol = 1)
+
+features
+
+reorder(Feature,-abs(elas.betas))
+
+View(cbind(features,ifelse(elas.betas>0,1,0)))
